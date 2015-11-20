07015d28fe8a8c9c9892fa687a3a687dd7549bc0
fattung79 <fattung@outlook.com>
2015-11-20 21:37:42 +0800

    Add Get-Enum function
    Get-Enum is an utility function to get an DictEnum object of a
    particular Enum Type

--- AX2009PS.psm1
+++ AX2009PS.psm1
@@ -91,6 +91,38 @@ Function Convert-FileName
     $fileLoc
 }
 
+Function Get-Enum
+{
+    <#
+
+    .SYNOPSIS
+
+    Create and returns an DictEnum object based on passed in Axapta object [initiated and connected] and the name of the Enum
+
+    .EXAMPLE
+
+    $ax = Get-AXObject
+    $enumABC = Get-Enum $ax "ABC"
+    
+    $enumABC.Call("name2Value","A")
+    1
+    
+    #>
+    
+    param(        
+        [Parameter(Mandatory = $true)]
+        [Microsoft.Dynamics.BusinessConnectorNet.Axapta] $ax,
+        
+        [Parameter(Mandatory = $true)]
+        [String] $enumName
+    )
+
+    $EnumObjNum = $ax.CallStaticClassMethod("Global","Enumname2id",$enumName)
+    $EnumObj = $ax.CreateAxaptaObject("DictEnum",$EnumObjNum)
+
+    $EnumObj
+}
+
 Function Get-AXObject
 {
     <#
